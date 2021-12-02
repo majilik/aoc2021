577f5a4e6fc40f5bd609659700e0fcad96161b84
David Gustavsson <majilik@live.se>
2021-12-02 12:44:42 +0100

    Day 2 p 1

---
+++ 2/main.ex
@@ -0,0 +1,49 @@
+defmodule Main do
+    def parse_file(filename) do
+        File.stream!(filename, [], :line)
+        |> Stream.map(&String.trim/1)
+        |> Stream.map(&parse_line/1)
+    end
+
+    def parse_line(line) do
+        line
+        |> String.split()
+        |> (fn ([direction, value]) -> {
+            String.to_atom(direction),
+            String.to_integer(value)
+        } end).()
+    end
+
+    def run1(filename) do
+        parse_file(filename)
+        |> solution1
+    end
+
+    def run2(filename) do
+        parse_file(filename)
+        |> solution2
+    end
+
+    def solution1(list) do
+        %{depth: depth, h_pos: h_pos} = list
+        |> Enum.reduce(%{depth: 0, h_pos: 0}, &run_command/2)
+        depth * h_pos
+    end
+
+    def run_command({:forward, value}, pos) do
+       %{h_pos: h_pos} = pos
+       %{pos | h_pos: h_pos + value} 
+    end
+    def run_command({:down, value}, pos) do
+        %{depth: depth} = pos
+        %{pos | depth: depth + value}
+    end
+    def run_command({:up, value}, pos) do
+        %{depth: depth} = pos
+        %{pos | depth: depth - value}
+    end
+
+    def solution2(list) do
+        list
+    end
+end
